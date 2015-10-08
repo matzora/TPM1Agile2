8169219f0060123fe848d40312dfe91b2697d1c1
PASCO Mathieu <mathieu.pasco@imie-rennes.fr>
2015-10-08 11:11:53 +0200

    #164 & #155, Design done, APIUtilities class started, method to log in added*

---
+++ c#/Mediatheque/LoginPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mediatheque
+{
+	/// <summary>
+	/// Logique d'interaction pour MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void BtnLogin_Click(object sender, RoutedEventArgs e)
+		{
+			string login = TextBoxLogin.Text;
+			string password = PasswordBoxLogin.Password;
+
+			APIUtilities.RequestLogin("http://192.168.1.41/api/login");
+			if(APIUtilities.LoginOk)
+			{
+				// Display the new view
+			}
+			else
+			{
+				// Display an error on this page
+			}
+		}
+	}
+}
